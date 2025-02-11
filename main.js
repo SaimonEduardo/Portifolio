564c840705393eb4c9bbf33551b901e5db2cf9cd
SaimonEduardo <eduardosaimon265@gmail.com>
2025-02-11 20:04:56 -0300

    Adicionando links e alterando nome de arquivos

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-// Seleciona todos os links de navegação
+
 const navLinks = document.querySelectorAll('nav a');
 
-// Para cada link, adiciona o comportamento de rolagem suave
+
 navLinks.forEach(link => {
     link.addEventListener('click', function (e) {
-        e.preventDefault(); // Impede o comportamento padrão de navegação
+        e.preventDefault(); 
 
-        // Obtém o destino da rolagem a partir do atributo href
-        const targetId = this.getAttribute('href').substring(1); // Remove o '#' de href
+       
+        const targetId = this.getAttribute('href').substring(1);
         const targetElement = document.getElementById(targetId);
 
-        // Rola suavemente para o elemento
+      
         targetElement.scrollIntoView({
             behavior: 'smooth',
             block: 'start'
